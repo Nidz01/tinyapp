6d12a2755f7e91668c5dd9013eff7c883ae70d80
Nidz01 <nidaemailid@gmail.com>
2020-07-07 03:43:05 +0000

    views

--- express_server.js
+++ express_server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs"); //Setting ejs as the view engine.
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true})); //The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the key body. 
+
+// A function that returns a string of 6 random alphanumeric characters
+const generateRandomString = function() {
+  return Math.random().toString(36).substr(2,8);
+};
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,9 +21,36 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// use res.render to load up an ejs view file
+// urls_index page 
+app.get('/urls', function(req, res) {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+
 // adding routes
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+// Add a GET Route to Show the Form of adding new URL
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//add a new URL taken by the previous get method on /urls/new 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+// Route to get new template of shortURL
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  console.log(templateVars);
+  res.render("urls_show", templateVars);
 });
 
 // response containing html code
